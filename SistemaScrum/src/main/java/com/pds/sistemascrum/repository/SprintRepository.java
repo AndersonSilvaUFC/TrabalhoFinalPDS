b33a7762c67bed9208c24e34ef60cd2a8b0a42d1
Felipe Sousa <85742339+FelpSousaS@users.noreply.github.com>
2022-07-04 20:57:32 -0300

    Repositório de Sprint

--- SistemaScrum/src/main/java/com/pds/sistemascrum/repository/SprintRepository.java
+++ SistemaScrum/src/main/java/com/pds/sistemascrum/repository/SprintRepository.java
@@ -1,10 +1,12 @@
 package com.pds.sistemascrum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 import com.pds.sistemascrum.model.Sprint;
 
-
+@Repository
 public interface SprintRepository extends JpaRepository<Sprint, Integer>{
 
 }
