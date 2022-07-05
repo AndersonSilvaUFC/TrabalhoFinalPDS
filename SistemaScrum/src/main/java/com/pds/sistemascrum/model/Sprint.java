53b46a5c7395fca06260db3595e96d8dd1d4fe33
Felipe Sousa <85742339+FelpSousaS@users.noreply.github.com>
2022-07-04 22:16:54 -0300

    classe Sprint

--- SistemaScrum/src/main/java/com/pds/sistemascrum/model/Sprint.java
+++ SistemaScrum/src/main/java/com/pds/sistemascrum/model/Sprint.java
@@ -1,5 +1,74 @@
 package com.pds.sistemascrum.model;
 
-public class Sprint {
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Sprint {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;	
+	
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
+	private Date dataInicial;
+	
+	@NotBlank
+	private Date dataFinal;
+	
+	@OneToMany
+	private List<Tarefa> tarefas;
+	
+	public Sprint(Integer id,@NotBlank String nome,@NotBlank Date dataInicial,@NotBlank Date dataFinal, List<Tarefa> tarefas) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.dataInicial = dataInicial;
+		this.dataFinal = dataFinal;
+		this.tarefas = tarefas;
+
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataInicial() {
+		return dataInicial;
+	}
+	public void setDataInicial(Date dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	public List<Tarefa> getTarefas() {
+		return tarefas;
+	}
+	public void setTarefas(List<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+	}
+
+	
+	
 }
